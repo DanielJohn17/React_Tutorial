1a6a9cbe90bc6c6aca57ec8c75efe680869a1e2e
DanielJohn17 <danielyohannes38@gmail.com>
2024-06-23 22:26:37 +0300

    Finished Features container and some minor fixes

--- gpt3-daniel/src/components/feature/Feature.jsx
+++ gpt3-daniel/src/components/feature/Feature.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 import "./feature.css";
 
 const Feature = ({ title, text }) => {
@@ -12,6 +14,11 @@ const Feature = ({ title, text }) => {
       </div>
     </div>
   );
+};
+
+Feature.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Feature;
