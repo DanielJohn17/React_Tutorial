844f46f4b6eda0b8edf4dae67a05fa7e4fc672a9
DanielJohn17 <danielyohannes38@gmail.com>
2024-06-23 22:03:12 +0300

    Finished whatGPT3 container, feature component with some minor fixes

--- gpt3-daniel/src/containers/header/Header.jsx
+++ gpt3-daniel/src/containers/header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         </p>
 
         <div className="gpt3__header-content__input">
-          <input type="email" placeholder="Your email Addres" />
+          <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
         </div>
 
